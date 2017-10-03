f3611e405ff6805cdec5494ebaa6465593998003
WNjihia <dorisn172@gmail.com>
2017-10-03 10:13:58 +0300

    add error handler

--- ckanext/datarequests/actions.py
+++ ckanext/datarequests/actions.py
@@ -25,14 +25,18 @@ import cgi
 import db
 import logging
 import validator
-from ckan.common import config
+import ckan.logic as logic
 import ckan.model as model
 import ckan.lib.base as base
 import ckan.lib.mailer as mailer
 
+from ckan.common import _, config
+from socket import error as socket_error
+
 c = plugins.toolkit.c
 log = logging.getLogger(__name__)
 tk = plugins.toolkit
+ValidationError = logic.ValidationError
 
 # Avoid user_show lag
 USERS_CACHE = {}
@@ -123,26 +127,44 @@ def _undictize_comment_basic(comment, data_dict):
     comment.datarequest_id = data_dict.get('datarequest_id', '')
 
 
-def datarequest_email_notification(context, result):
-    datarequest_url = config.get('ckan.site_url') + '/datarequest/' + result['id']
-    users = result['organization']['users']
-    extra_vars = {
-        'site_title': config.get('ckan.site_title'),
-        'site_url': config.get('ckan.site_url'),
-        'datarequest_title': result['title'],
-        'datarequest_description': result['description'],
-        'datarequest_url': datarequest_url,
-    }
-    subject = base.render_jinja2('emails/notify_user_subject.txt',
-                                 extra_vars)
+def datarequest_email_notification(context, data_dict):
+    '''
+    Action to send email notification to organization members when a new
+    data request is created.
 
-    for user in users:
-        # Retrieve user data
-        user_data = model.User.get(user['id'])
-        extra_vars['user_fullname'] = user_data.fullname
-        body = base.render_jinja2('emails/notify_user_body.txt',
-                                  extra_vars)
-        mailer.mail_user(user_data, subject, body)
+    :param id: The id of the data request
+    :type id: string
+    :param title: The title of the data request
+    :type title: string
+    :param description: A brief description for your data request
+    :type description: string
+    :param organization_users: The organization members who will be notified when a data request
+    :type organization_users: dictionary
+    '''
+    try:
+        datarequest_url = config.get('ckan.site_url') + '/datarequest/' + data_dict['id']
+        users = data_dict['organization']['users']
+        extra_vars = {
+            'site_title': config.get('ckan.site_title'),
+            'site_url': config.get('ckan.site_url'),
+            'datarequest_title': data_dict['title'],
+            'datarequest_description': data_dict['description'],
+            'datarequest_url': datarequest_url,
+        }
+        subject = base.render_jinja2('emails/notify_user_subject.txt',
+                                     extra_vars)
+
+        for user in users:
+            # Retrieve user data
+            user_data = model.User.get(user['id'])
+            extra_vars['user_fullname'] = user_data.fullname
+            body = base.render_jinja2('emails/notify_user_body.txt',
+                                      extra_vars)
+            mailer.mail_user(user_data, subject, body)
+    except(socket_error, mailer.MailerException) as error:
+        # Email could not be sent
+        msg = _('Error sending the notification email, {0}').format(error)
+        raise ValidationError({'message': msg}, error_summary=msg)
 
 
 def datarequest_create(context, data_dict):
